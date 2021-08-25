35407527caf6fa236c0f736715e8d26a30deea5c
don <misurakalu@gmail.com>
2021-08-25 11:35:35 +1000

    Practical one doing

---
+++ Practicals/Prac1/Prac1.m
@@ -0,0 +1,8 @@
+% Calculate the coeficients of the transfer function.
+
+
+L = 10 * 10^-3;  % Henry
+C = 100 * 10^-6; % farads
+R1 = 50 * 10^-3; % ohms
+R2 = 1;          % ohms
+
---
+++ Practicals/Prac1/Prac1_tips.m
@@ -0,0 +1,9 @@
+%% Practical 1 Tips
+%
+% When creating a transfer function keep the s^2 coefficient as a 1. If
+% it's not divide the equation to make it so.
+%
+% Numerator and the Denominator for the transfer function. Coe
+%
+% Using the out function so that matlab scripts can be used in conjunction
+% with the simulink
